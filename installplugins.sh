b1f7a3f1889054903f3ac48bf95b3da2ed1834de
Jan Clarin <janclarin@outlook.com>
2014-11-18 22:41:50 -0700

    fixed symbolic link command for .vimrc

--- installplugins.sh
+++ installplugins.sh
@@ -4,5 +4,6 @@ mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-# Creates a symbolic link to .vimrc file
-cp -s .vimrc ~/.vimrc
+# Replaces .vimrc in root directory with my .vimrc
+rm ~/.vimrc
+ln -s ~/vim-setup/.vimrc ~/.vimrc
